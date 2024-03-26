6b56cf2a65ee52ac615b517eb1ca1c762d3500a9
joshcrispo <c20356061@mytudublin.ie>
2024-03-26 23:28:28 +0000

    changed logic to only camera and fixed css

--- src/screens/HandTrackingScreen.js
+++ src/screens/HandTrackingScreen.js
@@ -23,6 +23,8 @@ const HandTrackingScreen = () => {
     const [isChatVisible, setIsChatVisible] = useState(false);
     const [isRecording, setIsRecording] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [countdown, setCountdown] = useState(null);
+
 
     const toggleChat = () => setIsChatVisible(!isChatVisible);
     const navigate = useNavigate();
@@ -51,73 +53,6 @@ const HandTrackingScreen = () => {
         });    
     };
 
-    const normalizeKeyPoints = (results, frameWidth, frameHeight) => {
-        
-        const pose = results.poseLandmarks ? results.poseLandmarks.map(lm => [lm.x, lm.y, lm.z, lm.visibility]) : new Array(33).fill([0, 0, 0, 0]);
-        // const face = results.faceLandmarks ? results.faceLandmarks.map(lm => [lm.x, lm.y, lm.z]) : new Array(468).fill([0, 0, 0]);
-        const lh = results.leftHandLandmarks ? results.leftHandLandmarks.map(lm => [lm.x, lm.y, lm.z]) : new Array(21).fill([0, 0, 0]);
-        const rh = results.rightHandLandmarks ? results.rightHandLandmarks.map(lm => [lm.x, lm.y, lm.z]) : new Array(21).fill([0, 0, 0]);
-
-        // Normalization
-        /*
-        [pose, face, lh, rh].forEach(keypoints => {
-            for (let i = 0; i < keypoints.length; i++) {
-                keypoints[i][0] /= frameWidth;
-                keypoints[i][1] /= frameHeight;
-            }
-        });
-        */
-
-        [pose, lh, rh].forEach(keypoints => {
-            for (let i = 0; i < keypoints.length; i++) {
-                keypoints[i][0] /= frameWidth;
-                keypoints[i][1] /= frameHeight;
-            }
-        });
-
-        // return [].concat(...pose, ...face, ...lh, ...rh).flat();
-        return [].concat(...pose, ...lh, ...rh).flat();
-    };
-
-    const handleModelSelect = (modelName) => {
-        setSelectedModel(modelName);
-    };
-
-    const handleBeginPrediction = () => {
-        setKeypointSequences([]);
-        setIsRecording(true);
-        setPredictionResult("Predicting...")
-        
-        setTimeout(() => {
-            setIsRecording(false);
-            handlePredict();
-        }, 5000);
-    };
-
-    const handlePredict = async () => {
-        console.log(keypointSequences.length);
-        console.log(selectedModel);
-        if (keypointSequences.length < 0) {
-            alert("Not enough data for prediction. Please wait until 30 sequences of keypoints are accumulated.");
-            return;
-        }
-        
-        try {
-            const response = await axios.post('http://127.0.0.1:7860/api/predict/', {
-                data: [
-                    selectedModel,
-                    JSON.stringify(keypointSequences.flat()) // Second input as a JSON string (or another format that matches your Gradio function's expectation)
-                ]
-            });
-            console.log("Prediction response:", response.data);
-            const resultString = response.data.data[0];
-            setPredictionResult(resultString); // Update the prediction result state
-        } catch (error) {
-            console.error("Error during prediction:", error);
-            setPredictionResult("Error during prediction.");
-        }
-    };
-
     useEffect(() => {
         document.title = "SignIT | Prediction"; 
       }, []);
@@ -130,100 +65,86 @@ const HandTrackingScreen = () => {
         return () => clearTimeout(timer);
     }, []);
     
+
     useEffect(() => {
-
-        const videoElement = videoRef.current;
-        const canvasElement = mainCanvasRef.current;
-        const canvasCtx = canvasElement.getContext('2d');
-
-
-        const holistic = new Holistic({
-            locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`,
-        });
-
-        holistic.setOptions({
-            modelComplexity: 1,
-            minDetectionConfidence: 0.5,
-            minTrackingConfidence: 0.5,
-        });
-
-        holistic.onResults(async (results) => {
-            canvasCtx.save();
-            canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-            // Only overwrite existing pixels.
-            canvasCtx.globalCompositeOperation = 'source-in';
-            canvasCtx.fillStyle = '#00FF00';
-            canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
-
-            // Only overwrite missing pixels.
-            canvasCtx.globalCompositeOperation = 'destination-atop';
-            canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-        
-            
-            canvasCtx.globalCompositeOperation = 'source-over';
-            drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                            {color: '#00FF00', lineWidth: 4});
-            //drawLandmarks(canvasCtx, results.poseLandmarks,
-            //                {color: '#FF0000', lineWidth: 2});
-            //drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
-            //                {color: '#C0C0C070', lineWidth: 1});
-            drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
-                            {color: '#CC0000', lineWidth: 5});
-            drawLandmarks(canvasCtx, results.leftHandLandmarks,
-                            {color: '#00FF00', lineWidth: 2});
-            drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
-                            {color: '#00CC00', lineWidth: 5});
-            drawLandmarks(canvasCtx, results.rightHandLandmarks,
-                            {color: '#FF0000', lineWidth: 2});
-            
-            canvasCtx.restore();
-
-            // Draw FPS
-            //const fpsCanvas = fpsCanvasRef.current;
-            //const fpsContext = fpsCanvas.getContext('2d');
-            //fpsContext.clearRect(0, 0, fpsCanvas.width, fpsCanvas.height);
-            //fpsContext.font = '30px Arial';
-            //fpsContext.fillStyle = 'lime'; // Neon color
-            //fpsContext.fillText(`FPS: ${fps}`, 10, 30);
-
-            // Normalize keypoints
-            const frameWidth = videoElement.videoWidth;
-            console.log("Width: ",frameWidth);
-            const frameHeight = videoElement.videoHeight;
-            console.log("Height: ", frameHeight);
-            const normalizedKeyPoints = normalizeKeyPoints(results, frameWidth, frameHeight);
-            accumulateKeypoints(normalizedKeyPoints); // Ensure this is called here
-
-            if (isRecording) {
-                const normalizedKeyPoints = normalizeKeyPoints(results, frameWidth, frameHeight);
-                accumulateKeypoints(normalizedKeyPoints);
+        const startCamera = () => {
+            const constraints = {
+                video: {
+                    width: 1280,
+                    height: 720
+                }
+            };
+    
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia(constraints)
+                    .then(stream => {
+                        videoRef.current.srcObject = stream;
+                        setIsLoading(false);
+                    })
+                    .catch(console.error);
             }
+        };
 
-            const nowInMs = Date.now();
-            const elapsed = nowInMs - lastTime;
-            lastTime = nowInMs;
+        startCamera();
+        return () => videoRef.current && videoRef.current.srcObject && videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+    }, []);
 
-            // Calculate FPS
-            fps = Math.round(1000 / elapsed);
-            
-        }); 
-        
-        const camera = new Camera(videoElement, {
-        onFrame: async () => {
-            await holistic.send({ image: videoElement });
-        },
-        width: 1280,
-        height: 720,
-        });
+    const captureFrames = async () => {
+        const frames = [];
+        const canvas = document.createElement('canvas');
+        canvas.width = videoRef.current.videoWidth;
+        canvas.height = videoRef.current.videoHeight;
+        const ctx = canvas.getContext('2d');
 
-        camera.start();
+        for (let i = 0; i < 40; i++) {
+            ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+            frames.push(canvas.toDataURL('image/jpeg'));
+            await new Promise(resolve => setTimeout(resolve, 125)); // 125ms interval for 8fps
+        }
 
-        return () => {
-        camera.stop();
-        holistic.close();
+        return frames;
     };
-}, []);
+
+    const handleModelSelect = (modelName) => {
+        setSelectedModel(modelName);
+    };
+
+    const handleBeginPrediction = async () => {
+        if (isLoading) {
+            alert('Camera is not ready yet.');
+            return;
+        }
+
+        // Implement countdown from 3 to 1 before capturing frames
+        for (let i = 3; i > 0; i--) {
+            setCountdown(i); // Update countdown state
+            await new Promise(resolve => setTimeout(resolve, 1000)); // 1-second intervals
+        }
+        setCountdown(null); // Hide countdown
+
+        setPredictionResult("Capturing frames...");
+        const frames = await captureFrames();
+
+        setPredictionResult("Predicting...");
+        handlePredict(frames);
+    };
+
+    const handlePredict = async () => {
+        try {
+            const response = await axios.post('http://127.0.0.1:7860/api/predict/', {
+                data: [
+                    selectedModel,
+                    JSON.stringify(frames)                
+                ]
+            });
+            console.log("Prediction response:", response.data);
+            const resultString = response.data.data[0];
+            setPredictionResult(resultString); // Update the prediction result state
+        } catch (error) {
+            console.error("Error during prediction:", error);
+            setPredictionResult("Error during prediction.");
+        }
+    };
 
 return (
     <div className='hand-tracking-screen'>
@@ -252,18 +173,9 @@ return (
                     </div>
                     <div className="video-content">
                         
-                        <video style={{ transform: 'scaleX(-1)' }} autoPlay ref={videoRef} />
-                        {isCanvasVisible && (
-                            // Ensure the canvas covers the video and is also mirrored
-                            <canvas style={{ 
-                                top: 0,
-                                left: 0,
-                                transform: 'scaleX(-1)',
-                                width: '100%',
-                                height: '100%'
-                            }} ref={mainCanvasRef} width={1280} height={720} />
-                        )}
-
+                        <video ref={videoRef} style={{ transform: 'scaleX(-1)' }} autoPlay playsInline />
+                        {/* Display countdown if it's not null */}
+                        {countdown !== null && <p className="countdown">{countdown}</p>}
                         {/* FPS canvas without mirroring effect 
                         <canvas style={{
                             position: 'absolute',
